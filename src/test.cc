59f70594b4ddc588144b1363cc5d4bcf25dfe486
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2016-09-11 03:00:51 +0000

    Refactor test

--- src/test.cc
+++ src/test.cc
@@ -1,17 +1,28 @@
 #include "ephemeral_store.h"
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
 
 int main(){
 
+    std::vector<std::pair<Id, Data>> test_data;
+
+    /* Generate a large amount of test data */
+    test_data.push_back(std::pair<Id, Data>(Id("Test_Key"), Data("Test_Data")));
+
+    /* Test and time */
+
     Ephemeral_Store es;
 
-    Id id("Test_Key");
-    Data data("Test_Data");
+    for(const auto &d: test_data){
+        es.write(d.first, d.second);
+    }
 
-    es.write(id, data);
-
-    std::cout << es.read(id).data() << std::endl;
+    for(const auto &d: test_data){
+        std::cout << es.read(d.first).data() << std::endl;
+    }
 
     return 0;
 };
