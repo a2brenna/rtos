83f349b6b9e4504cadef15de1b3ba95d435324cd
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2016-09-21 10:34:00 +0000

    Update TODO

--- src/fs_store.cc
+++ src/fs_store.cc
@@ -30,30 +30,39 @@ std::string FS_Store::_find(const Id &id) const{
 void FS_Store::store(const Id &id, const Data &data){
     const std::string p = _find(id);
     struct stat s;
-    const auto r = stat( p.c_str(), &s);
+    const auto r = stat( p.c_str(), &s); //TODO: investigate other return codes
     if( r == 0 ){
         throw E_EXISTS();
     }
     else{
         std::ofstream f;
         f.open(p);
+        //TODO: check for errors?
         f << data.data();
+        //TODO: check for errors?
         f.close();
+        //TODO: check for errors?
     }
 }
 
 void FS_Store::append(const Id &id, const Data &data){
     std::ofstream f;
     f.open(_find(id), std::ios::app);
+    //TODO: check for errors?
     f << data.data();
+    //TODO: check for errors?
     f.close();
+    //TODO: check for errors?
 }
 
 Data FS_Store::fetch(const Id &id) const{
     std::ifstream f;
     f.open(_find(id));
+    //TODO: check for errors?
     std::string s;
     f >> s;
+    //TODO: check for errors?
     f.close();
+    //TODO: check for errors?
     return Data(s);
 }
