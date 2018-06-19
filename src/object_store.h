6d0a346915365665aef0a891dd0913994786ce31
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2018-06-19 09:47:44 -0400

    Add more PLAN tech, more TODOs and revamp API

--- src/object_store.h
+++ src/object_store.h
@@ -9,21 +9,23 @@ class E_OBJECT_DNE {};
 
 class E_DATA_DNE {};
 
+struct Stats {
+    size_t size;
+};
+
 class Object_Store {
 
     public:
 
-        virtual void store(const Ref&id, const Object &data) = 0;
-        virtual void append(const Ref&id, const Object &data) = 0;
-        virtual void append(const Ref&id, const char *data, const size_t &size) = 0;
-        virtual Object fetch_head(const Ref&id, const size_t &num_bytes) const = 0;
-        virtual Object fetch_tail(const Ref&id, const size_t &num_bytes) const = 0;
-        virtual void fetch_head(const Ref&id, const size_t &num_bytes, char *buf) const = 0;
-        virtual void fetch_tail(const Ref&id, const size_t &num_bytes, char *buf) const = 0;
-        virtual Object fetch(const Ref&id) const = 0;
-        virtual Object fetch_from(const Ref&id, const size_t &start) const = 0;
-        virtual Object fetch(const Ref&id, const size_t &start, const size_t &num_bytes) const = 0;
-        virtual void fetch(const Ref&id, const size_t &start, const size_t &num_bytes, char *buf) const = 0;
+        virtual void create(const Ref &read_id, const Ref &write_id, const Ref &rm_id) = 0;
+        virtual void remove(const Ref &rm_id) = 0;
+
+        virtual void append(const Ref &write_id, const Ref &read_id, const uint64_t &index, const Object &data) = 0;
+        virtual void append(const Ref &write_id, const Object &data) = 0;
+        virtual void mutate(const Ref &target_read_id, const Ref &target_write_id, const Ref &target_rm_id, const Ref &source_read_id, const int64_t &index, const Object &data) = 0;
+
+        virtual Object read(const Ref &read_id, const int64_t &index, const size_t &num_bytes) const = 0;
+        virtual Stats stat(const Ref &read_id) = 0;
 
 };
 #endif
