c116dc3a3404545298a076e265c602471f4fd08d
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2018-07-05 17:08:10 -0400

    Pare down Ephemeral_Store definition

--- src/ephemeral_store.cc
+++ src/ephemeral_store.cc
@@ -1,18 +1,1 @@
 #include "ephemeral_store.h"
-
-void Ephemeral_Store::store(const Ref&id, const Object &data){
-    _store[id] = data;
-}
-
-void Ephemeral_Store::append(const Ref&id, const Object &data){
-    _store[id].append(data);
-}
-
-Object Ephemeral_Store::fetch(const Ref&id) const{
-    try{
-        return _store.at(id);
-    }
-    catch(std::out_of_range e){
-        throw E_OBJECT_DNE();
-    }
-}
--- src/ephemeral_store.h
+++ src/ephemeral_store.h
@@ -3,17 +3,11 @@
 
 #include "object_store.h"
 
-#include <map>
-
 class Ephemeral_Store : public Object_Store {
 
     public:
-        void store(const Ref&id, const Object &data);
-        void append(const Ref&id, const Object &data);
-        Object fetch(const Ref&id) const;
 
     private:
-        std::map<Ref, Object> _store;
 
 };
 #endif
